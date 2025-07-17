f32cbeb12028b87b4aac43b02f5f25c0955330f2
ondistantshores7 <ondistantshores@optonline.net>
2025-07-17 12:18:54 -0400

    Fix: Correct asset path case-sensitivity

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -3,11 +3,11 @@ export class BootScene extends Phaser.Scene {
         super({ key: 'BootScene' });
     }
     preload() {
-        this.load.image('bird1', 'assets/bird1.png');
-        this.load.image('bird2', 'assets/bird2.png');
-        this.load.image('bird3', 'assets/bird3.png');
+        this.load.image('bird1', 'assets/Bird_01.png');
+        this.load.image('bird2', 'assets/Bird_02.png');
+        this.load.image('bird3', 'assets/Bird_03.png');
         this.load.image('pipe', 'assets/pipe.png');
-        this.load.image('cloud', 'assets/cloud.png');
+        this.load.image('cloud', 'assets/Cloud.png');
     }
     create() {
         this.scene.start('MainMenuScene');
